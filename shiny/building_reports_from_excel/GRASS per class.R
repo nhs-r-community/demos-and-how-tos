52a68b74c08f64098ec0fcab0ed1e1177b115e72
Chris Beeley <chris.beeley@gmail.com>
2021-06-20 20:58:03 +0100

    Edit filepaths

--- shiny/building_reports_from_excel/GRASS per class.R
+++ shiny/building_reports_from_excel/GRASS per class.R
@@ -14,41 +14,38 @@ library(tidyverse)
 library(readxl)
 library(gridExtra)
 
-df <- read_excel("data/TestScoresDB.xlsx", sheet=1)
-df$marks_perc <- df$marks/df$max_marks*100
+df <- read_excel("data/TestScoresDB.xlsx", sheet = 1)
+df$marks_perc <- df$marks / df$max_marks * 100
 
 df$subject <- factor(tolower(df$subject))
 df$test_desc <- factor(tolower(df$test_desc))
-df$name_short <- factor(df$name_short)
+df$name_short <- factor(df$name_short2)
 df$test_occ_no_f <- factor(paste("Occ", df$test_occ, sep=":"))
 
-if (!dir.exists("plots")) {
-  dir.create("plots")
-}
-
-df <- df[df$class==WhichClass,]
+df <- df[df$class == WhichClass, ]
 
 admission_numbers <- unique(df$adm_no)
 
 n_subjects <- length(unique(levels(df$subject)))
 n_pupils <- length(unique((df$adm_no)))
 
-for (i in 1:n_pupils) {
-  xdf <- df[df$adm_no==admission_numbers[i],]
+for (i in 1 : n_pupils) {
+  xdf <- df[df$adm_no == admission_numbers[i], ]
   for(j in 1:n_subjects) {
-    my_file_name <- unique(paste(	xdf$name_short[xdf$adm_no==admission_numbers[i]], "AdmNo", 	
-                                  xdf$adm_no[xdf$adm_no==admission_numbers[i]], 
-                                  "Class", xdf$class[xdf$adm_no==admission_numbers[i]]),sep="-")		
-    my_file_name <- paste(my_file_name, sep="-")
-    print(my_file_name)
-    pdf (paste(my_file_name, "pdf", sep='.'), width=15, height=10)
-    p1 <- ggplot(xdf, aes(x=test_occ_no_f, y=marks_perc, group=name_short)) +
-      geom_line()+
-      geom_point()+	
-      facet_wrap(subject~test_desc)+
-      ylab("% Score")+
-      xlab("Test Occ")+
-      scale_y_continuous(breaks = seq(0, 100, 10), limits=c(0,100))+
+    my_file_name <- unique(
+      paste0(xdf$name_short[xdf$adm_no==admission_numbers[i]], "_AdmNo_", 	
+            xdf$adm_no[xdf$adm_no==admission_numbers[i]], 
+            "_Class_", xdf$class[xdf$adm_no==admission_numbers[i]]), ".pdf")
+    
+    pdf (file.path("plots", my_file_name), width = 15, height = 10)
+    
+    p1 <- ggplot(xdf, aes(x = test_occ_no_f, y = marks_perc, group = name_short)) +
+      geom_line() +
+      geom_point() +	
+      facet_wrap(subject ~ test_desc) +
+      ylab("% Score") +
+      xlab("Test Occ") +
+      scale_y_continuous(breaks = seq(0, 100, 10), limits = c(0, 100)) +
       ggtitle(my_file_name)
     print(p1)
     dev.off()
