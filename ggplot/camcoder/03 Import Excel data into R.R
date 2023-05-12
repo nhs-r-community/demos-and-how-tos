de8660d91ce180c28d4924d23bd3a894c30c45bc
Chris Beeley <chris.beeley@gmail.com>
2023-05-12 10:56:50 +0100

    PR edit (#55)
    * Create ggplot2-visualizations.txt
    
    Added ggplot2-visualizations description
    
    * Create visualizations_description.txt
    
    Added list of plots to ggplot2-visualizations project
    
    * Update and rename ggplot2-visualizations.txt to ggplot2-visualizations/ggplot2-visualizations.txt
    
    Updated list of plots included in this project
    
    * Add files via upload
    
    Uploaded different ggplot2 examples
    
    * Create Camcoder.txt
    
    Added folder for camcoder
    
    * Add files via upload
    
    Added step by step example on how to use Camcoder
    
    * Create Data_files_plots.txt
    
    Added raw data files to ggplot2 visualizations project
    
    * Add files via upload
    
    Added csv and Excel files used to create GGPLOT2 charts
    
    * Create .gitignore
    
    Added .gitignore file to ggplot2-visualizations project and containing  each individual .png file to be ignored
    
    * Update .gitignore
    
    Added all .png files to .gitignore file
    
    * Move files into ggplot2 folder
    
    * Remove images
    
    * Change stupid Windows filenames
    
    * Update .gitignore
    
    * Tidying up code comments
    
    * Ammended code to improve its quality
    
    * Amended code to improve its quality
    
    * Amended code to improve quality
    
    * Amended code to improve its quality 
    
    Amended code to improve its quality
    
    * Amended code to improve quality
    
    * Created new “data” folder and transfer all files from previous Data folder  onto new “data”  folder. Then deleted Data folder once it is empty.
    
    * Amended code to improve its quality
    
    * Amended code to improve its quality
    
    * Resolved conflicts
    
    ---------
    
    Co-authored-by: Pablo Leon <datavizvalencia@gmail.com>
    Co-authored-by: Zoë Turner <zoe.turner3@nhs.net>

---
+++ ggplot/camcoder/03 Import Excel data into R.R
@@ -0,0 +1,116 @@
+# 03 Import Excel data into R.R 
+
+# Load required packages at once (readxl,here,dplyr,janitor) 
+
+library(readxl)
+library(here)
+library(dplyr)
+library(janitor)
+
+
+# Added "where_am_i" across all instances an absolute path is required 
+where_am_i <- here::here()
+
+excel_file <- list.files (paste0(where_am_i,"/ggplot2-visualizations/data"),pattern = "xlsx$")
+excel_file
+
+# [1] "RTT_TS_data.xlsx"
+excel_tabs <- excel_sheets(paste0(where_am_i,"/ggplot2-visualizations/data/RTT_TS_data.xlsx"))
+excel_tabs
+
+# We read in data from Excel using {readxl} package
+# From {readxl} package we use the read_excel function to read in data from Excel file
+
+# Parameters
+# sheet = number [Number of sheet to be imported]
+# skip = number [Number of rows from the top of the file to be skipped when importing data into Excel]
+# range = "C10:F18" [Range of rows from a specific sheet to be Imported into R]
+# na = ""   [How missing values are defined in the input file "-", "#" ]
+
+# Two examples
+# Skip certain rows of data: 
+# Myocardial_infarction <- read_excel(
+#              here("data", "CCG_1.17_I01968_D.xlsx"), 
+#              sheet = 3, skip =13) %>% 
+#              clean_names()
+# How to select rows of data 
+#Tab10202in <- read_excel(here("Input_files",DataA),sheet = 1,range = "C10:F18",skip = 1,na = "")
+
+### Importing our main RTT data
+
+# There are 9 Rows of data in the Excel file we downloaded from the URL
+# File name "RTT_TS_data.xlsx"
+# skip = 9
+
+# Let's try to import it just by specifying the number of rows to omit
+
+
+
+# 1-3 First get the File name we want to import 
+excel_file <- list.files (paste0(where_am_i,"/ggplot2-visualizations/data"),pattern = "xlsx$")
+excel_file
+# [1] "RTT_TS_data.xlsx"
+
+# 2-3 Then get the Tab names to choose which one to import (with multi tab files)
+excel_tabs <- excel_sheets(paste0(where_am_i,"/ggplot2-visualizations/data/RTT_TS_data.xlsx"))
+excel_tabs
+#[1] "Full Time Series"
+
+# Start applying all these parameters to our function
+RTT_Data <- read_excel(paste0(where_am_i,"/ggplot2-visualizations/data/RTT_TS_data.xlsx"),sheet = "Full Time Series")
+RTT_Data
+
+# 1. Add argument to skip first 10 rows of data
+# So this will get us the right Table headings
+RTT_Data <- read_excel(paste0(where_am_i,"/ggplot2-visualizations/data/RTT_TS_data.xlsx"),sheet = "Full Time Series",
+                       skip = 10)
+RTT_Data
+
+names(RTT_Data)
+
+# 2.Add na argument to get rid of missing values  na = "-"  
+# In this particular example, missing values are defined by "-" character
+# Try to adjust the spaces for the missing values
+RTT_Data <- read_excel(paste0(where_am_i,"/ggplot2-visualizations/data/RTT_TS_data.xlsx"),sheet = "Full Time Series",
+                       skip = 10 , na = "-")
+
+RTT_Data
+
+# 3. Use {Janitor} package to get clear names using "clear_names()"function
+# This file worked fine and solves:
+# a. Importing null values from original file defined as "-"
+# b. Cleaning original variable names using clean_names() function from Janitor package
+RTT_Data <- read_excel(paste0(where_am_i,"/ggplot2-visualizations/data/RTT_TS_data.xlsx"),sheet = "Full Time Series",
+                       skip = 10 , na = "-") %>% 
+  clean_names()
+
+RTT_Data
+# Try to capture that missing value better
+Variable_names <- names(RTT_Data)
+Variable_names
+
+# 4. As we can see we have plenty of variables, we will start by subsetting them and keeping just TWO
+
+# x2 that will correspond to "Date" and "Total waiting(Mil)" That corresponds to Total figure of incomplete pathways or waiting list
+RTT_data_sub <- RTT_Data %>% 
+  select(x2,total_waiting_mil)
+RTT_data_sub    
+
+# Now rename the variable appropriately
+TT_data <- RTT_data_sub %>% select(Date = x2, Total_waiting = total_waiting_mil)
+RTT_data
+
+# We can also remote null values using drop_na() function rom tidyr package
+# From {dplyr} package we can use na.omit() function
+RTT_data <- RTT_data_sub %>% 
+  select(Date = x2, Total_waiting_M = total_waiting_mil) %>% 
+  na.omit()
+RTT_data
+
+# Check how the data looks like
+install.packages("tidyverse",dependencies = TRUE)
+library(tidyverse)
+
+RTT_data_plot <- RTT_data %>% 
+  ggplot(X = Date, Y = Total_waiting_M, aes()) + 
+  geom_line()
---
+++ ggplot/camcoder/Data/README.md
@@ -0,0 +1,1 @@
+This folder contains all files used in the ggplot2-visualizations project
